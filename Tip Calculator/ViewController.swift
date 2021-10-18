c48bcfa28019049f0f37291e1304a8fb90a8954b
Yijie Guo <yguo1@macalester.edu>
2021-10-18 08:25:48 +0800

    Finished the tip calculator
    Implemented the basic functions. Also includes the number pad entry.

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,  UITextFieldDelegate{
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var totalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        billAmountTextField.delegate = self
+    }
+    
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        updateLabel(textField: textField)
+
     }
 
-
+    @IBAction func calculateTip(_ sender: Any) {
+        updateLabel(textField: billAmountTextField)
+    }
+    
+    func updateLabel(textField: UITextField){
+        let bill = Double(textField.text!) ?? 0
+        let tipPercentages = [0.15, 0.18, 0.2]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        tipAmountLabel.text=String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+    }
+    
 }
 
